b6be244188bde3dbe4606ac5361986947320b936
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-13 19:08:30 +0000

    No matter what boss is boss!

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,14 @@
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   let iterable = [];
   if (!report.allEmployees || typeof report.allEmployees !== 'object') {
-	  return iterable;
+  return iterable;
   }
   iterable = {
-	  * [Symbol.iterator] () {
-		  for (const value of Object.values(report.allEmployees)) {
-			  for (const i of value) {
-				  yield i;
-        }
+  [Symbol.iterator]() {
+  for (const value of Object.values(report.allEmployees)) {
+  for (const i of value) {
+  yield i;
+	  }
       }
     }
   };
